ec1fe754116844078ea7c6fbb5c914a0c8515ec1
Xreath <samed_fazli@hotmail.com>
2022-09-27 00:47:34 +0300

    Core Data usage finished

---
+++ Alisveris/DetailViewController.swift
@@ -0,0 +1,138 @@
+//
+//  DetailViewController.swift
+//  Alisveris
+//
+//  Created by Fazlı Koç on 26.09.2022.
+//
+
+import UIKit
+import CoreData
+
+class DetailViewController: UIViewController ,UIImagePickerControllerDelegate,UINavigationControllerDelegate{
+    
+    @IBOutlet weak var productSize: UITextField!
+    @IBOutlet weak var productPrice: UITextField!
+    @IBOutlet weak var productName: UITextField!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    var selectedName =  ""
+    var selectedUUID : UUID?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print(selectedName)
+        if selectedName != "" {
+            
+            
+            if let UUIDString = selectedUUID?.uuidString {
+                let appDelegate = UIApplication.shared.delegate as! AppDelegate
+                let context = appDelegate.persistentContainer.viewContext
+                let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Shop")
+                fetchRequest.predicate = NSPredicate(format: "id = %@", UUIDString)
+                fetchRequest.returnsObjectsAsFaults = false
+                
+                do{
+                    let result = try context.fetch(fetchRequest)
+                    if result.count > 0 {
+                        for i in result as! [NSManagedObject]{
+                            if  let name = i.value(forKey: "name") as? String{
+                                productName.text=name
+                            }
+                            
+                            if  let price = i.value(forKey: "price") as? Double{
+                                
+                                productPrice.text = String(price)
+                            }
+                            
+                            if  let size = i.value(forKey: "size") as? String{
+                                productSize.text = String(size)
+                            }
+                            
+                            if  let imageData = i.value(forKey: "image") as? Data{
+                                let uiImage=UIImage(data: imageData)
+                                imageView.image = uiImage
+                            }
+                            
+                        }
+                        
+                        
+                    }
+                }
+                catch{
+                    print("ID'den çekerken hata oldu")
+                }
+                
+            }
+        }
+        else{
+            productName.text=""
+            productSize.text=""
+            productPrice.text=""
+        }
+        
+        let gestureRecognizer=UITapGestureRecognizer(target: self, action: #selector(keybordClose))
+        
+        view.addGestureRecognizer(gestureRecognizer)
+        
+        view.isUserInteractionEnabled = true
+        let imageRecognizer=UITapGestureRecognizer(target: self, action: #selector(imageSelector))
+        
+        view.addGestureRecognizer(imageRecognizer)
+    }
+    @objc func imageSelector(){
+        let picker=UIImagePickerController()
+        picker.delegate=self
+        picker.sourceType = .photoLibrary
+        picker.allowsEditing=false
+        present(picker, animated: true)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        imageView.image=info[.originalImage] as? UIImage
+        self.dismiss(animated: true)
+    }
+    
+    @objc func keybordClose(){
+        view.endEditing(true)
+    }
+    
+    
+    
+    @IBAction func saveAction(_ sender: Any) {
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context=appDelegate.persistentContainer.viewContext
+        
+        let shop = NSEntityDescription.insertNewObject(forEntityName: "Shop", into: context)
+        
+        shop.setValue(productName.text!, forKey:"name")
+        
+        shop.setValue(productSize.text!, forKey:"size")
+        
+        
+        if let priceInt = Int(productPrice.text!){
+            shop.setValue(priceInt, forKey:"price")
+        }
+        
+        shop.setValue(UUID(), forKey: "id")
+        
+        
+        let binaryImage = imageView.image!.jpegData(compressionQuality: 0.5)
+        
+        shop.setValue(binaryImage, forKey: "image")
+        
+        
+        do{
+            
+            try
+            context.save()
+            print("Save Process Complate")
+            
+        }
+        catch{print("OPSS a error")
+        }
+        NotificationCenter.default.post(name: NSNotification.Name(rawValue: "insert"), object: nil)
+        self.navigationController?.popViewController(animated: true)
+    }
+}
--- Alisveris/ViewController.swift
+++ Alisveris/ViewController.swift
@@ -6,14 +6,99 @@
 //
 
 import UIKit
+import CoreData
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+    
+    var nameArray = [String]()
+    var idArray = [UUID]()
+    
+    var selectName = ""
+    var selectUUID :UUID?
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        NotificationCenter.default.addObserver(self, selector: #selector(fetchData), name: NSNotification.Name(rawValue: "insert"), object: nil)
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        tableView.delegate=self
+        tableView.dataSource=self
+        
+        navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.add, target: self, action: #selector(addButton))
+        
+        fetchData()
     }
-
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return nameArray.count    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell=UITableViewCell()
+        cell.textLabel?.text=nameArray[indexPath.row]
+        return cell
+    }
+    
+    @objc func addButton(){
+        
+        selectName = ""
+        performSegue(withIdentifier: "toDetailVC", sender: nil)
+    }
+    
+    @objc func fetchData(){
+        
+        nameArray.removeAll(keepingCapacity: false)
+        idArray.removeAll(keepingCapacity: false)
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context=appDelegate.persistentContainer.viewContext
+        
+        let fetchRequest=NSFetchRequest<NSFetchRequestResult>(entityName: "Shop")
+        fetchRequest.returnsObjectsAsFaults = false
+        
+        do{
+            let results = try  context.fetch(fetchRequest)
+            if results.count > 0 {
+                
+                for i in results as! [NSManagedObject]{
+                    if  let name = i.value(forKey: "name") as? String{
+                        nameArray.append(name)
+                    }
+                    
+                    if  let id = i.value(forKey: "id") as? UUID{
+                        idArray.append(id)
+                    }
+                    
+                }
+                tableView.reloadData()
+                
+            }
+            
+           
+        }
+        catch{
+            print("Hata Oluştu")
+        }
+        
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        selectName=nameArray[indexPath.row]
+        selectUUID=idArray[indexPath.row]
+        performSegue(withIdentifier: "toDetailVC", sender: nil)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "toDetailVC"{
+            let destinationVC=segue.destination as! DetailViewController
+            destinationVC.selectedName=selectName
+            destinationVC.selectedUUID=selectUUID
+        }
+    }
+    //Hello everybody
 }
 
